e643f149f0e001ff722cb9dc5b6577eebe230d11
Kimball31 <kimball@kimballrexford.com>
2023-09-21 15:52:22 -0400

    Update forminator-paypal-description-from-url.php
    use forminator_get_form_name(form_id)

--- forminator-paypal-description-from-url.php
+++ forminator-paypal-description-from-url.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* Plugin Name: [Forminator] - PayPal Order Description From URL.
-* Description: Add description to PayPal Transaction based on form url.
+* Plugin Name: [Forminator] - PayPal Order Description From Form ID.
+* Description: Add description to PayPal Transaction based on form id.
 * Plugin URI: https://github.com/Kimball31/Forminator-PayPal-Order-Description-From-URL
 * Author: Kimball Rexford
 * Author URI: https://kimballrexford.com/
-* Version: 0.2
+* Version: 0.3
 * License: GPLv2 or later
 * Readme: To install copy file to /wp-content/mu-plugins/ folder
 */
@@ -17,35 +17,22 @@ if ( ! defined( 'ABSPATH' ) || ( defined( 'WP_CLI' ) && WP_CLI ) ) {
 add_filter( 'forminator_paypal_create_order_request', 'forminator_mu_paypal_description_from_path', 100, 2 );
 
 function forminator_mu_paypal_description_from_path( $request, $data ) {
-    parse_str( $data['form_fields'], $form_fields );
     
-    // default description set to the domain name (without schema or path)
-    $description = parse_url( get_site_url(), PHP_URL_HOST );
-
-    // make description from the forms path
-    if ( isset( $form_fields['current_url'] ) ) {
-
-        // get form path for making description
-        $form_url_path = parse_url( $form_fields['current_url'], PHP_URL_PATH );
-
-        // remove everything that is not text or numbers from path
-        $description = preg_replace( '/[^a-zA-Z0-9]/', ' ', $form_url_path );  
-
-        // remove extra spaces from description and trim
-        $description = trim( preg_replace( '/\s+/', ' ', $description ) ); 
-
-        // capitalize words in description
-        $description = ucwords( strtolower( $description ) );            
-
-    }
+    date_default_timezone_set('US/Eastern');
     
-    // cut description at 127 characters max, per spec at https://developer.paypal.com/docs/api/orders/v2/
-    if( strlen( $description ) > 127 ) {
-        $description = substr( $description, 0, 127 );     
+    // defaults:
+    $description = '(missing)'; 
+    $form_id = 0;
+
+    if(isset($data['form_id'])){
+        $form_id = $data['form_id'];
+        $form_name = forminator_get_form_name($form_id);
+        $description = $form_name;        
     }        
 
-    // set paypal description 
-    $request['purchase_units'][0]['description'] = $description;     
+    // set paypal order description 
+    $request['purchase_units'][0]['description'] = $description;   
+    $request['purchase_units'][0]['reference_id'] = $form_id;  //external ID for the purchase unit
      
     return $request;
 }
